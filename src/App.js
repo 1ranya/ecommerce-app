16bd50c06634949f8293012792dd486da925aabd
TOUNSI Rania <rania.tounsi@ouidou.fr>
2021-04-07 19:24:47 +0100

    Add Single Product Page + Home + Router

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@
 import React from 'react';
 import data from "./data"
 import Product from "./components/Product"
+import { BrowserRouter, Route } from 'react-router-dom';
+import Homescreen from './screens/HomeScreen';
+import Productscreen from './screens/ProductScreen';
 function App() {
 	console.log(data)
   return (
-    <div className="grid-container">
-		<header className="row">
-			<div>
-				<a className="brand" href="/">She Can Code</a>
-			</div>
-			<div>
-				<a href="/cart">Cart</a>
-				<a href="/signin">Sign In </a>
-			</div>
-		</header>
-		<main>
-			<div>
-				<div className="row center">
-					{data.products.map(product =>(
-						<Product key={product._id} product={product}/>
-					))}
+	<BrowserRouter>
+		<div className="grid-container">
+			<header className="row">
+				<div>
+					<a className="brand" href="/">She Can Code</a>
 				</div>
-			</div>
-		</main>
-		<footer className="row center">
-				All rights reserved!
-		</footer>
-	</div>
+				<div>
+					<a href="/cart">Cart</a>
+					<a href="/signin">Sign In </a>
+				</div>
+			</header>
+			<main>
+				<Route path="/" component={Homescreen} exact></Route>
+				<Route path="/product/:id" component={Productscreen}></Route>
+			</main>
+			<footer className="row center">
+					All rights reserved!
+			</footer>
+		</div>
+	</BrowserRouter>
   );
 }
 
